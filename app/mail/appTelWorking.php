e098739b12237efe3714bd2e9fb5c6a48ac70ce9
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-01-11 13:57:41 +0200

    Continue Working with gulp. Add New page.

---
+++ app/mail/appTelWorking.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Создание формы обратной связи</title>
+<meta http-equiv="Refresh" content="4; URL=https://workgroup.in.ua/working.html">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+<?php
+
+// сохраняем в переменную данные полученные из поля c именем
+$userservice = $_POST['service'];
+$username = $_POST['customerName'];
+$usertel = $_POST['telephone'];
+
+// ФИЛЬТРЫ
+
+// преобразует все символы, которые пользователь попытается добавить в форму
+$userservice =htmlspecialchars($userservice);
+$username =htmlspecialchars($username);
+$usertel =htmlspecialchars($usertel);
+
+
+// декодирует url, если пользователь попытается его добавить в форму.
+$userservice = urldecode($userservice);
+$username = urldecode($username);
+$usertel = urldecode($usertel);
+
+//удалим пробелы с начала и конца строки, если таковые имеются
+$userservice = trim($userservice);
+$username = trim($username);
+$usertel = trim($usertel);
+
+
+
+// echo $userservice;
+// echo "<br>";
+// echo $username, $usertel ;
+
+if (mail(
+	"workgroup8sender@workgroup.in.ua",
+	"Заявка с сайта",
+	" тип запроса:  ". $userservice .
+	"| имя: ". $username ." | телефон: ".$usertel ,
+	"From: workgroup8sender@workgroup.in.ua \r\n"))
+
+ { reset; 	echo "<center><img src='../image/spasibo.png'></center>";}
+
+
+else {echo "<center><img src='../image/ne-otprevleno.png'></center>";}
+
+
+?>
+
+</body>
+</html>
+
